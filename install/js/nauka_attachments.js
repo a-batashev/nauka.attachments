1b993a2d6f7151b5e50cd313ca3d612d772573f6
Arkharis <arkharis@ya.ru>
2019-01-09 15:11:02 +0300

    Edited nauka_attachments.js

--- install/js/nauka_attachments.js
+++ install/js/nauka_attachments.js
@@ -1,28 +1,55 @@
 BX.addCustomEvent('OnEditorInitedBefore', function() {
-	
-	console.log('test', BX('form_element_1_form'));
-	
 	var _this = this;
 	this.AddButton({
-		src: '/bitrix/images/nauka.attachments/nauka_attachments.png', // icon
+		src: '/bitrix/images/nauka.attachments/nauka_attachments.png', // button icon
 		id: 'nauka_attachments', // button id
 		name: BX.message('NAUKA_ATTACHMENTS_BUTTON_TITLE'), // button title
-		handler: function() {
-			BX.ajax({
-				method: 'POST',
-				url: '/bitrix/admin/nauka_attachments_list.php',
-				dataType: 'html',
-				data: { 'ID': text },
-				onsuccess: function(response) {
-					if (response.length > 0) {
-						_this.SetContent(response, true);
-					}
-				}
+		handler: function() { // button onclick
+			
+			// Take post data from URL
+			var content_post = window.location.search.match(/((IBLOCK_ID|ID)=\d+)/g).join("&");
+			
+			var NaukaDialog = new BX.CDialog({
+				title: BX.message('NAUKA_ATTACHMENTS_DIALOG_TITLE'),
+				//content: "No files attached.", //BX.findChildByClassName(BX('tr_PROPERTY_5808'), 'adm-detail-content-cell-r'),
+				content_url: '/bitrix/admin/nauka_attachments_list.php',
+				content_post: content_post,
+				width: 600,
+				height: 600,
+				buttons:
+				[
+					/*{
+						title: BX.message('NAUKA_ATTACHMENTS_DIALOG_PASTE'),
+						id: 'nauka_paste',
+						action: function() {
+							
+							console.log(this);
+							
+							this.parentWindow.Close();
+						},
+					},*/
+					BX.CDialog.prototype.btnClose
+				]
 			});
+			NaukaDialog.Show();
+			
+			BX.bindDelegate(
+				BX(NaukaDialog.GetContent()),
+				'click',
+				{
+					className: 'insert-files'
+				},
+				function () {
+					console.log(this);
+					_this.selection.InsertHTML(this.innerHTML);
+				}
+			);
+			
+			//console.log(BX(NaukaDialog.GetContent()));
 			
 			
-			$html = '<p>TEST</p>'
-			_this.selection.InsertHTML($html);
+			//$html = '<p>TEST</p>'
+			//_this.selection.InsertHTML($html);
 		}
 	});
 });
